3ee7b9e6b972858c80aadeb05a57588443c666e6
Chandan Ojha <chandanojha776@gmail.com>
2024-01-16 17:41:58 +0600

    ##like-dislike-functionality-implementation

--- resources/js/components/barta/Barta.vue
+++ resources/js/components/barta/Barta.vue
@@ -3,6 +3,8 @@ import { ref } from "vue";
 import { router, Link, usePage } from "@inertiajs/vue3";
 import { format } from "date-fns";
 import { flashMessage } from "../../utils/functions";
+import LikeButton from "./LikeButton.vue";
+import DislikeButton from "./DislikeButton.vue";
 const showMenu = ref(0);
 const page = usePage();
 
@@ -20,6 +22,7 @@ const toggleMenu = (bartaId) => {
     }
 };
 
+//barta delete
 function barta_delete(bartaId) {
     if (confirm("Are you sure you want to delete this barta?")) {
         router.delete(`/barta-delete/${bartaId}`, {
@@ -32,7 +35,7 @@ function barta_delete(bartaId) {
             },
         });
     }
-};
+}
 </script>
 
 <template>
@@ -168,48 +171,10 @@ function barta_delete(bartaId) {
             <div class="flex items-center justify-between">
                 <div class="flex gap-6 text-gray-600">
                     <!-- Like Button -->
-                    <Link
-                        href="#"
-                        type="button"
-                        class="-m-2 flex gap-2 text-xs items-center rounded-full p-2 text-gray-600 hover:text-gray-800"
-                    >
-                        <span class="sr-only">Like</span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 24 24"
-                            stroke-width="2"
-                            stroke="currentColor"
-                            class="w-5 h-5">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
-                        </svg>
-
-                        <p>5</p>
-                    </Link>
+                    <LikeButton :barta="barta" />
                     <!-- /Like Button -->
                     <!-- Dislike Button -->
-                    <Link
-                        href="#"
-                        type="button"
-                        class="-m-2 flex gap-2 text-xs items-center rounded-full p-2 text-gray-600 hover:text-gray-800"
-                    >
-                        <span class="sr-only">Dislike</span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 24 24"
-                            stroke-width="2"
-                            stroke="currentColor"
-                            class="w-5 h-5">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M7.498 15.25H4.372c-1.026 0-1.945-.694-2.054-1.715a12.137 12.137 0 0 1-.068-1.285c0-2.848.992-5.464 2.649-7.521C5.287 4.247 5.886 4 6.504 4h4.016a4.5 4.5 0 0 1 1.423.23l3.114 1.04a4.5 4.5 0 0 0 1.423.23h1.294M7.498 15.25c.618 0 .991.724.725 1.282A7.471 7.471 0 0 0 7.5 19.75 2.25 2.25 0 0 0 9.75 22a.75.75 0 0 0 .75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 0 0 2.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384m-10.253 1.5H9.7m8.075-9.75c.01.05.027.1.05.148.593 1.2.925 2.55.925 3.977 0 1.487-.36 2.89-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398-.306.774-1.086 1.227-1.918 1.227h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 0 0 .303-.54" />
-                        </svg>
-
-                        <p>1</p>
-                    </Link>
+                    <DislikeButton :barta="barta" />
                     <!-- /Dislike Button -->
                     <!-- Comment Button -->
                     <Link
@@ -247,14 +212,17 @@ function barta_delete(bartaId) {
                         <span class="sr-only">Share</span>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            fill="none" viewBox="0 0 24 24"
+                            fill="none"
+                            viewBox="0 0 24 24"
                             stroke-width="2"
                             stroke="currentColor"
-                            class="w-5 h-5">
+                            class="w-5 h-5"
+                        >
                             <path
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
-                                d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+                                d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+                            />
                         </svg>
 
                         <p>3</p>
