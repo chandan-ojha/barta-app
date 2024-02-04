f68dbb8672152d8f638c6ab293f381868773fc17
Chandan Ojha <chandanojha776@gmail.com>
2024-02-04 16:21:39 +0600

    #install-tailwind-css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     {{-- <title>Barta App</title> --}}
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <!-- tailwindcss cdn -->
-    <script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <!-- google font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"/>
+    @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     @inertiaHead
     <style>
         * {
-          font-family: "Inter", sans-serif;
+            font-family: "Inter", sans-serif;
         }
     </style>
-  </head>
-  <body class="bg-gray-100">
-    @inertia
-  </body>
+</head>
+<body class="bg-gray-100">
+@inertia
+</body>
 </html>
